88823e567254530a6b00a27193da44472848c2e1
Benjamin Legrand <android@legrand.ws>
2017-02-06 12:01:57 +0100

    We also need to be a little bit more intrusive and change the path of the download torrent so that it also targets the server. Server hostname/schema needs to be provided as a command line parameter

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"encoding/xml"
 
@@ -12,14 +14,14 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Provide port to run on")
+	if len(os.Args) != 3 {
+		fmt.Printf("Provide port to run on and server basename")
 		os.Exit(1)
 	}
 	port := os.Args[1]
 
 	e := echo.New()
-	e.GET("/*", func(c echo.Context) error {
+	e.GET("/rss/*", func(c echo.Context) error {
 
 		res, err := http.Get(fmt.Sprintf("https://zooqle.com%s?%s", c.Request().URL.Path, c.Request().URL.RawQuery))
 		if err != nil {
@@ -30,6 +32,7 @@ func main() {
 		f := FXML{}
 		zDec := xml.NewDecoder(res.Body)
 		fEnc := xml.NewEncoder(c.Response())
+		c.Response().Write([]byte(xml.Header))
 
 		err = zDec.Decode(&z)
 		if err != nil {
@@ -39,12 +42,21 @@ func main() {
 		f.Channel.Items = make([]FItem, len(z.Channel.Items))
 		for i, zi := range z.Channel.Items {
 			f.Channel.Items[i] = FItem{
-				Title: zi.Title,
+				Title: strings.Replace(zi.Title, "–", "-", -1),
 				Link:  zi.Link,
-				GUID:  zi.GUID.URL,
+				GUID:  strings.Replace(zi.GUID.URL, "https://zooqle.com", os.Args[2], -1),
 			}
 		}
 		err = fEnc.Encode(&f)
+		return err
+	})
+	e.GET("/download/*", func(c echo.Context) error {
+		res, err := http.Get(fmt.Sprintf("https://zooqle.com%s?%s", c.Request().URL.Path, c.Request().URL.RawQuery))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = io.Copy(c.Response(), res.Body)
 		return err
 	})
 	e.Logger.Fatal(e.Start(":" + port))
